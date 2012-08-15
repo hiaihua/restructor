114caaafcd2ba22c9a41c9dd2274338ce1943307
George Schneeloch <bostonbusmap@gmail.com>
2012-08-15 11:36:19 -0400

    borrowing code from the SfM library

--- src/testApp.h
+++ src/testApp.h
@@ -31,11 +31,35 @@ class testApp : public ofBaseApp{
 		ofPtr<ofxCv::Flow> flow;
 
 		cv::Mat fundamentalMatrix;
+		cv::Mat essentialMatrix;
 		ofImage disparity;
-		cv::StereoBM stereoBM;
 	private:
 		ofPtr<ofxCv::Flow> initializePyrLK();
 		ofPtr<ofxCv::Flow> initializeFarneback();
+		std::vector<cv::Point3d> pointcloud;
+		std::vector<cv::KeyPoint> correspImg1Pt;
+
+		std::vector<cv::KeyPoint> convertFrom(const std::vector<cv::Point2f>& points);
+		double TriangulatePoints(const vector<cv::KeyPoint>& pt_set1,
+							   const std::vector<cv::KeyPoint>& pt_set2,
+							   const cv::Mat& Kinv,
+							   const cv::Mat& distcoeff,
+							   const cv::Matx34d& P,
+							   const cv::Matx34d& P1,
+							   std::vector<cv::Point3d>& pointcloud,
+							   std::vector<cv::KeyPoint>& correspImg1Pt);
+		cv::Mat_<double> IterativeLinearLSTriangulation(cv::Point3d u,	//homogenous image point (u,v,1)
+				cv::Matx34d P,			//camera 1 matrix
+				cv::Point3d u1,			//homogenous image point in 2nd camera
+				cv::Matx34d P1			//camera 2 matrix
+													);
+		cv::Mat_<double> LinearLSTriangulation(cv::Point3d u,		//homogenous image point (u,v,1)
+				cv::Matx34d P,		//camera 1 matrix
+										   cv::Point3d u1,		//homogenous image point in 2nd camera
+										   cv::Matx34d P1		//camera 2 matrix
+										   );
+
+
 };
 
 #endif	
